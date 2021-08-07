772b413a633b4098cbea48d87c3964112d7fb068
sagun-k <sagunkshrestha7@gmail.com>
2021-08-07 16:59:23 +0545

    last commit

---
+++ assets/index.36943f1f.js
@@ -0,0 +1,10 @@
+function displaymenu(){
+    document.getElementById('ham').style.display="none";
+    document.getElementById('close').style.display="inline-block";
+    document.getElementById('menu').style.display="inline-block";
+}
+function closemenu(){
+    document.getElementById('ham').style.display="block";
+    document.getElementById('close').style.display="none";
+    document.getElementById('menu').style.display="none";
+}
